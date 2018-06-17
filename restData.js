02aea47ecea39a1541da1e338091450d8218672c
Dima Savenkov <savenkovdb@gmail.com>
2018-06-17 19:00:10 +0300

    Add restData.js file with dummy data

---
+++ restData.js
@@ -0,0 +1,17 @@
+module.exports = function () {
+    let data = {
+        employees: [
+            { id: 1, firstName: "Dima", lastName: "Savenkov", isActive: 1, emp_depID: 1 },
+            { id: 2, firstName: "Peter", lastName: "Sermon", isActive: 0, emp_depID: 2 },
+            { id: 3, firstName: "Den", lastName: "Brown", isActive: 0, emp_depID: 3 },
+            { id: 4, firstName: "John", lastName: "Doe", isActive: 1, emp_depID: 1 },
+            { id: 5, firstName: "Den", lastName: "Baydakov", isActive: 1, emp_depID: 2 },
+            { id: 6, firstName: "Peter", lastName: "Newman", isActive: 0, emp_depID: 1 },
+            { id: 7, firstName: "Alex", lastName: "Pavlov", isActive: 1, emp_depID: 1 },
+            { id: 8, firstName: "David", lastName: "Stewart", isActive: 0, emp_depID: 1 },
+            { id: 9, firstName: "Kate", lastName: "Kolesnikova", isActive: 1, emp_depID: 1 },
+            { id: 10, firstName: "Ann", lastName: "Golovan", isActive: 1, emp_depID: 1 }
+        ]
+    }
+    return data
+}
